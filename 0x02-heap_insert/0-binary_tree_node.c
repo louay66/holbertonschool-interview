aed35d0b17dde9873d490e3bdabbbc4c42a78ab1
louay66 <louaylouay20@gmail.com>
2022-12-22 00:24:26 +0100

    new node

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - add a node to the tree
+ *
+ * @parent: parent node
+ * @value: value  node
+ *
+ * Return: The height of the tree starting at @node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = value;
+	if (parent)
+		new_node->parent = parent;
+	else
+		new_node->parent = NULL;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
