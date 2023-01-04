66a39fa2a8680996084811a4e81bf0eefbf0a2d9
louay66 <louaylouay20@gmail.com>
2023-01-05 00:15:36 +0100

    0-minoperations.py

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,25 +9,16 @@ def minOperations(n):
     method that calculates the fewest operations
     """
     newvalue = n
+    i = 2
     minm = 0
-    if not isinstance(n, int):
+    if (not isinstance(n, int)):
         return 0
     if n < 2:
         return 0
     while newvalue != 1:
-        if newvalue % 2 == 0:
-            newvalue = newvalue / 2
-            minm += 2
-        elif newvalue % 3 == 0:
-            newvalue = newvalue / 3
-            minm += 3
-        elif newvalue % 5 == 0:
-            newvalue = newvalue / 5
-            minm += 5
-        elif newvalue % 7 == 0:
-            newvalue = newvalue / 7
-            minm += 7
+        if newvalue % i == 0:
+            newvalue = newvalue / i
+            minm += i
         else:
-            minm += newvalue
-            newvalue = 1
-    return int(minm)
+            i = i + 1        
+    return minm
