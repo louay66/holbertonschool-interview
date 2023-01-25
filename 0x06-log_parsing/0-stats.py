beb7208e11d07bb0305af6d2162f3063bf8b0b12
louay66 <louaylouay20@gmail.com>
2023-01-25 17:24:17 +0100

    0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,36 +1,41 @@
 #!/usr/bin/python3
-""" program that read line from stdin"""
-import fileinput
+"""Input stats"""
+import sys
+
+stats = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+sizes = [0]
 
 
-def print_output():
-    """ print the status acording"""
-    print("File size: {}".format(file_size))
-    for a, b in sorted(status_code.items()):
-        if b != 0:
-            print("{}: {}".format(a, b))
+def print_stats():
+    print('File size: {}'.format(sum(sizes)))
+    for s_code, count in sorted(stats.items()):
+        if count:
+            print('{}: {}'.format(s_code, count))
 
 
-if __name__ == "__main__":
-    """main function"""
-
-    status_code = {'200': 0, '301': 0, '400': 0,
-                   '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
-    file_size = 0
-    count = 0
-    try:
-        for i in fileinput.input():
-            try:
-                data = i.split(" ")
-                if data[7] in status_code.keys():
-                    status_code[data[7]] += 1
-                file_size += int(data[8])
-                count += 1
-            except Exception:
-                pass
-            if count % 10 == 0:
-                print_output()
-        print_output()
-    except KeyboardInterrupt:
-        print_output()
-        raise
+try:
+    for i, line in enumerate(sys.stdin, start=1):
+        matches = line.rstrip().split()
+        try:
+            status_code = matches[-2]
+            file_size = matches[-1]
+            if status_code in stats.keys():
+                stats[status_code] += 1
+            sizes.append(int(file_size))
+        except Exception:
+            pass
+        if i % 10 == 0:
+            print_stats()
+    print_stats()
+except KeyboardInterrupt:
+    print_stats()
+    raise
