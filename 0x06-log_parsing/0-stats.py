72c08854629f6d230705da89c918596a24cd5b1d
louay66 <louaylouay20@gmail.com>
2023-01-25 16:38:07 +0100

    0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     status_code = {'200': 0, '301': 0, '400': 0,
                    '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
     file_size = 0
-    count = 1
+    count = 0
     try:
         for i in fileinput.input():
             if count % 10 == 0 and count != 0:
