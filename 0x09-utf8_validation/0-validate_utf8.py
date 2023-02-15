446e08473cfa41dd00a08805b59f697cdccd9f43
louay66 <louaylouay20@gmail.com>
2023-02-15 16:29:23 +0100

    0-validate_utf8.py

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -9,16 +9,16 @@ def validUTF8(data):
     count_byte = 0
     for i in data:
         if count_byte == 0:
-            if (i >> 5) == 6:
+            if (i >> 5) == 0b110:
                 count_byte = 1
-            elif (i >> 4) == 14:
+            elif (i >> 4) == 0b1110:
                 count_byte = 2
-            elif (i >> 3) == 30:
+            elif (i >> 3) == 0b11110:
                 count_byte = 3
             elif (i >> 7):
                 return False
         else:
-            if (i >> 6) != 2:
+            if (i >> 6) != 0b10:
                 return False
             count_byte -= 1
     return count_byte == 0
