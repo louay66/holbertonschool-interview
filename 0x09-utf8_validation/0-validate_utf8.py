e9ab71fb96ed387a8cb2e6091dcb206d492a02f0
louay66 <louaylouay20@gmail.com>
2023-02-15 16:31:14 +0100

    0-validate_utf8.py

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -6,19 +6,22 @@ utf-8 ot note
 
 
 def validUTF8(data):
+    """valid the list of int if thare is character in
+    utf-8 ot note
+    """
     count_byte = 0
     for i in data:
         if count_byte == 0:
-            if (i >> 5) == 0b110:
+            if (i >> 5) == 6:
                 count_byte = 1
-            elif (i >> 4) == 0b1110:
+            elif (i >> 4) == 14:
                 count_byte = 2
-            elif (i >> 3) == 0b11110:
+            elif (i >> 3) == 30:
                 count_byte = 3
             elif (i >> 7):
                 return False
         else:
-            if (i >> 6) != 0b10:
+            if (i >> 6) != 2:
                 return False
             count_byte -= 1
     return count_byte == 0
