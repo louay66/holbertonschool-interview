0fce5b779c68d157d7093b3a374302d33d8bed43
louay66 <louaylouay20@gmail.com>
2023-03-09 15:57:39 +0100

    test

--- 0x0B-menger/menger.h
+++ 0x0B-menger/menger.h
@@ -1,6 +1,10 @@
 #ifndef MENGER_H_
 #define MENGER_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 void menger(int level);
 void remove_center(int size, char sponge[][size]);
 void print_sponge(int size, char sponge[][size]);
