91bcfd2fe977cc08908d6cd0ed4c7ac163a6cb16
louay66 <louaylouay20@gmail.com>
2023-03-09 16:10:19 +0100

    0-menger.c

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -8,56 +8,37 @@
  */
 void menger(int level)
 {
-	int i, j, k, p, q;
+	int i_size = pow(3, level);
+
+	int i_row = 0, i_rhigh = 0, i_colum = 0, i_chigh = 0;
+	char c_print = '#';
 
 	if (level < 0)
 	{
 		return;
 	}
 
-	int size = pow(3, level);
-	char sponge[size][size];
-
-	for (i = 0; i < size; i++)
+	for (; i_colum < i_size; i_colum++)
 	{
-		for (j = 0; j < size; j++)
+		for (i_row = 0; i_row < i_size; i_row++)
 		{
-			sponge[i][j] = '#';
-		}
-	}
-	int center = size / 3;
-
-	for (i = center; i < center * 2; i++)
-	{
-		for (j = center; j < center * 2; j++)
-		{
-			sponge[i][j] = ' ';
-		}
-	}
-	for (k = 0; k < level; k++)
-	{
-		int step = pow(3, k);
-
-		for (i = step; i < size; i += step * 3)
-		{
-			for (j = step; j < size; j += step * 3)
+			c_print = '#';
+			i_rhigh = i_row;
+			i_chigh = i_colum;
+			while (i_rhigh > 0 && i_chigh > 0 /* edge case */)
 			{
-				for (p = 0; p < step; p++)
+				if (i_rhigh % 3 == 1 && i_chigh % 3 == 1)
 				{
-					for (q = 0; q < step; q++)
-					{
-						sponge[i + p][j + q] = ' ';
-					}
+					c_print = ' ';
 				}
+
+				i_rhigh /= 3;
+				i_chigh /= 3;
 			}
+
+			printf("%c", c_print);
 		}
-	}
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			printf("%c", sponge[i][j]);
-		}
+
 		printf("\n");
 	}
 }
