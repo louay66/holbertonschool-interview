3e4de540fae8a97a84aeb6e443c84d28fb251b92
louay66 <louaylouay20@gmail.com>
2023-07-05 11:03:55 +0100

    iforget to execute file

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Simple python code to determine the fewest
+"""
+Simple python code to determine the fewest
 number of coins needed to meet agiven amount " total ".
 """
 
@@ -9,17 +10,17 @@ def makeChange(coins, total):
     Determines the fewest number of coins needed to meet a given amount total.
     """
     if total <= 0:
-        return (0)
+        return 0
     coins.sort()
     coins.reverse()
     fewest = 0
     i = 0
     while (i < len(coins)):
         if total == 0:
-            return (fewest)
+            return fewest
         if coins[i] > total:
             i += 1
         else:
             total -= coins[i]
             fewest += 1
-    return (-1)
+    return -1
