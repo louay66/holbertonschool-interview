6e51ebe2124f3de46172708b8f27e3593dc0260d
louay66 <louaylouay20@gmail.com>
2023-07-12 14:58:23 +0100

    0-add_node.c

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -1,8 +1,52 @@
+#include <stdlib.h>
+#include <string.h>
 #include "list.h"
 
 /**
- * add_node_end - Add a new node at the begining
- * of a double circular linked list
+ * add_node_end - Add a new node to the end of a double circular linked list
+ * @list: Pointer to the list to modify
+ * @str: The string to copy into the new node
+ *
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *new_node;
+	List *last;
+
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (!(new_node->str))
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	if (*list == NULL)
+	{
+		new_node->prev = new_node;
+		new_node->next = new_node;
+		*list = new_node;
+	}
+	else
+	{
+		last = (*list)->prev;
+
+		new_node->prev = last;
+		new_node->next = *list;
+
+		last->next = new_node;
+		(*list)->prev = new_node;
+	}
+
+	return (new_node);
+}
+
+/**
+ * add_node_begin - Add a new node to a double circular linked list
  * @list: Pointer to the list to modify
  * @str: The string to copy into the new node
  *
@@ -10,45 +54,36 @@
  */
 List *add_node_begin(List **list, char *str)
 {
-	return (add_node_end(list, str) ? *list = (*list)->prev : NULL);
-}
-/**
- * add_node_end - Add a new node at the end of a double circular linked list
- * @list: Pointer to the list to modify
- * @str: The string to copy into the new node
- *
- * Return: Address of the new node, or NULL on failure
- */
+	List *new_node;
+	List *last;
 
-List *add_node_end(List **list, char *str)
-{
-	List *end, *new;
-
-	if (!list || !str)
-		return (NULL);
-	new = malloc(sizeof(*new));
-	if (!new)
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
 		return (NULL);
 
-	new->str = strdup(str);
-	if (!new->str)
+	new_node->str = strdup(str);
+	if (!(new_node->str))
 	{
-		free(new);
+		free(new_node);
 		return (NULL);
 	}
-	if (!(*list))
+	if (*list == NULL)
 	{
-		*list = new;
-		new->next = new->prev = new;
+		new_node->prev = new_node;
+		new_node->next = new_node;
 	}
 	else
 	{
-		end = (*list)->prev;
+		last = (*list)->prev;
 
-		new->prev = end;
-		new->next = (*list);
-		end->next = (*list)->prev = new;
+		new_node->prev = last;
+		new_node->next = *list;
+
+		last->next = new_node;
+		(*list)->prev = new_node;
 	}
 
-	return (new);
+	*list = new_node;
+
+	return (new_node);
 }
