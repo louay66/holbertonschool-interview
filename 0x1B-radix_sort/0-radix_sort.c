ab9d6c4f6dc6a5cb893ce33939520e2ba0518aa6
louay66 <louaylouay20@gmail.com>
2023-07-19 13:44:20 +0100

    fix betty

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -10,16 +10,16 @@
  */
 int getMax(int *array, size_t size)
 {
-   int max = array[0];
-   size_t i;
+	int max = array[0];
+	size_t i;
 
-   for (i = 1; i < size; i++)
-   {
-      if (array[i] > max)
-         max = array[i];
-   }
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
 
-   return (max);
+	return (max);
 }
 
 /**
@@ -30,26 +30,26 @@ int getMax(int *array, size_t size)
  */
 void countingSort(int *array, int size, int exp)
 {
-   int *output = malloc(size * sizeof(int));
-   int count[10] = {0};
-   int i;
+	int *output = malloc(size * sizeof(int));
+	int count[10] = {0};
+	int i;
 
-   for (i = 0; i < size; i++)
-      count[(array[i] / exp) % 10]++;
+	for (i = 0; i < size; i++)
+		count[(array[i] / exp) % 10]++;
 
-   for (i = 1; i < 10; i++)
-      count[i] += count[i - 1];
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
 
-   for (i = size - 1; i >= 0; i--)
-   {
-      output[count[(array[i] / exp) % 10] - 1] = array[i];
-      count[(array[i] / exp) % 10]--;
-   }
+	for (i = size - 1; i >= 0; i--)
+	{
+		output[count[(array[i] / exp) % 10] - 1] = array[i];
+		count[(array[i] / exp) % 10]--;
+	}
 
-   for (i = 0; i < size; i++)
-      array[i] = output[i];
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
 
-   free(output);
+	free(output);
 }
 
 /**
@@ -59,23 +59,25 @@ void countingSort(int *array, int size, int exp)
  */
 void radix_sort(int *array, size_t size)
 {
-   int max = getMax(array, size);
-   int exp;
-   size_t i;
+	int max = getMax(array, size);
+	int exp;
+	size_t i;
 
-   for (exp = 1; max / exp > 0; exp *= 10)
-   {
-      countingSort(array, size, exp);
+	if (!array || size <= 0)
+		return;
+	for (exp = 1; max / exp > 0; exp *= 10)
+	{
+		countingSort(array, size, exp);
 
-      for (i = 0; i < size; i++)
-         if (i == size - 1)
-         {
-            printf("%d", array[i]);
-         }
-         else
-         {
-            printf("%d, ", array[i]);
-         }
-      printf("\n");
-   }
+		for (i = 0; i < size; i++)
+			if (i == size - 1)
+			{
+				printf("%d", array[i]);
+			}
+			else
+			{
+				printf("%d, ", array[i]);
+			}
+		printf("\n");
+	}
 }
